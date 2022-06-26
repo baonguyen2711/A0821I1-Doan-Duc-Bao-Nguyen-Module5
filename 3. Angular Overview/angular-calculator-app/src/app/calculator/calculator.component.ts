a3eccaf6bdd68197a38fa2a63afc0972f2272551
myusername <baonguyendn2711@gmail.com>
2022-06-27 00:48:57 +0700

    Hello

--- 3. Angular Overview/angular-calculator-app/src/app/calculator/calculator.component.ts
+++ 3. Angular Overview/angular-calculator-app/src/app/calculator/calculator.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-calculator',
@@ -6,10 +6,39 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./calculator.component.css']
 })
 export class CalculatorComponent implements OnInit {
-
+  num = 1;
+  number1 = 0;
+  number2 = 0;
+  choice = "";
+  result = 0;
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  clear() {
+    this.number1 = 0;
+    this.number2 = 0;
+  }
+
+  checkChoice(checkChoice:string) {
+    this.choice = checkChoice;
+  }
+
+  getResult() {
+    switch (this.choice) {
+      case "+":
+        this.result = this.number1 + this.number2;
+        break;
+      case "-":
+        this.result = this.number1 - this.number2;
+        break;
+      case "*":
+        this.result = this.number1 * this.number2;
+        break
+      case  "/":
+        this.result = this.number1 / this.number2;
+        break;
+    }
+  }
 }
